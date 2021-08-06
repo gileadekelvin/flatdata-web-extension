1586bb7c3e84b31c8eadf70af5c105228613fa75
gileadekelvin <gileadekelvin@gmail.com>
2021-08-06 17:38:39 -0300

    Modify content script to render flat viewer button with redirect working

--- background.js
+++ background.js
@@ -4,3 +4,16 @@ chrome.runtime.onInstalled.addListener(() => {
   chrome.storage.sync.set({ color });
   console.log('Default background color set to %cgreen', `color: ${color}`);
 });
+
+chrome.tabs.onUpdated.addListener(
+  function(tabId, changeInfo, tab) {
+    // read changeInfo data and do something with it
+    // like send the new url to contentscripts.js
+    if (changeInfo.url) {
+      chrome.tabs.sendMessage( tabId, {
+        message: 'hello!',
+        url: changeInfo.url
+      })
+    }
+  }
+);
--- src/popup/content.js
+++ src/popup/content.js
@@ -1,34 +1,71 @@
-// alert("Hello from your Chrome extension!")
+// TEST IF CONTENT SCRIPT IS WORKING
 
-console.log('FlatData Extension working')
+// console.log("FlatData Extension working");
 
-let paragraphs = document.getElementsByTagName('p')
+// let paragraphs = document.getElementsByTagName("p");
 
-for (elem of paragraphs) {
-  elem.style['background-color'] = 'green'
-}
+// for (elem of paragraphs) {
+//   elem.style["background-color"] = "green";
+// }
+
 // INIT
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // listen for messages sent from background.js
+  if (request.message === "hello!") {
+    console.log(request.url); // new url is now in content scripts!
+    addFlatDataButton();
+  }
+});
 
-// get Owner and Repository
-const owner = document.getElementsByClassName('author')[0].outerText
-const repository = document.querySelectorAll('[itemprop="name"]')[0].outerText;
+const addFlatDataButton = () => {
+  // get Owner and Repository
+  const owner = document.getElementsByClassName("author")[0].outerText;
+  const repository =
+    document.querySelectorAll('[itemprop="name"]')[0].outerText;
 
-// Get commit sha
-const commitURL = document.getElementsByClassName('text-small text-mono Link--secondary')[0].href;
-const parsedCommitSha = commitURL.match(/commit\/(.*)/)[1]
+  // Get commit sha
+  const commitURL = document.getElementsByClassName(
+    "text-small text-mono Link--secondary"
+  )[0];
+  const parsedCommitSha = commitURL
+    ? commitURL.href.match(/commit\/(.*)/)[1]
+    : "";
 
-// Get path to the data inside repository
-const tabURL = window.location.href;
-const parsedDataPath = tabURL.match(/blob\/(.*)/)[1].match(/\/(.*)/)[1];
+  // Get path to the data inside repository
+  const tabURL = window.location.href;
+  const parsedDataPath = tabURL.match(/blob\/(.*)/)[1].match(/\/(.*)/)[1];
 
-// Button definition
-const btn = document.createElement('BUTTON')
-const t = document.createTextNode('Flat Data Viewer');
-btn.appendChild(t);
-btn.onclick = function () {
-  const url = `https://flatgithub.com/${owner}/${repository}?filename=${parsedDataPath}&sha=${parsedCommitSha}`
-  window.open(url, '_blank').focus();
-  return false;
-}
+  // Button definition
+  const btn = document.createElement("DIV");
 
-document.body.appendChild(btn);
+  //append icon
+  const elem = document.createElement("img");
+  elem.setAttribute("src", "https://flatgithub.com/assets/flat.630c008a.svg");
+  elem.setAttribute("height", "18");
+  elem.setAttribute("width", "18");
+  elem.setAttribute("alt", "");
+  elem.setAttribute("class", "octicon pr-1");
+  btn.appendChild(elem);
+
+  //append text
+  const t = document.createTextNode("Flat Viewer");
+  btn.appendChild(t);
+  btn.className = "btn btn-sm";
+
+  //define onclick
+  btn.onclick = function () {
+    const url = `https://flatgithub.com/${owner}/${repository}?filename=${parsedDataPath}&sha=${parsedCommitSha}`;
+    window.open(url, "_blank").focus();
+    return false;
+  };
+
+  // add button to div
+  const btnGroup = document.getElementsByClassName(
+    "d-flex py-1 py-md-0 flex-auto flex-order-1 flex-md-order-2 flex-sm-grow-0 flex-justify-between hide-sm hide-md"
+  )[0];
+  console.log("btnGroup", btnGroup);
+
+  btnGroup.appendChild(btn);
+};
+
+addFlatDataButton();
